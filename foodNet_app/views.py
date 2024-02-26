db426563037c3f369b3a50de74940279fe5b52f5
geekynice <nicebanjaraa@gmail.com>
2024-02-26 15:41:34 -0500

    user model creation

--- foodNet_app/views.py
+++ foodNet_app/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import User, UserModel
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
 
-def register(request):
-    return render(request, 'register.html')
-
 def login(request):
     return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        name = request.POST.get('first_name')
+        password = request.POST.get('last_name')
+        profile_pic = request.POST.get('last_name')
+        photo_id = request.POST.get('last_name')
+        DOB = request.POST.get('last_name')
+        address = request.POST.get('last_name')
+        is_donor = request.POST.get('last_name')
+        user = User.objects.create(name=name,
+                                    password=password,
+                                    )
+        userModel = UserModel.objects.create(profile_pic = profile_pic,
+                                    photo_id = photo_id,
+                                    DOB = DOB,
+                                    address = address,
+                                    is_donor = is_donor,
+                                    user = user)
+        userModel.save()
+        user.save()
+        
+        return redirect('index')
+    else:
+        return render(request, 'register.html')
+
