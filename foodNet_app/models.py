0df6cece15de793e4504ca1c7c45ac7b377b4a91
geekynice <nicebanjaraa@gmail.com>
2024-02-26 14:12:39 -0500

    sign up forms

--- foodNet_app/models.py
+++ foodNet_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profile_pic/')
+    photo_id = models.ImageField(upload_to='photo_id/')
+    DOB = models.DateField() # must be 18 years or older
+    address = models.CharField(max_length=200)
+    is_donor = models.BooleanField(default=True)
+
+    def clean(self):
+        age = timezone.now().date().year - self.DOB.year - ((timezone.now().date().month, timezone.now().date().day) < (self.DOB.month, self.DOB.day))
+        if age < 18:
+            raise ValidationError("Users must be 18 years or older to register.")
+
+    def __str__(self):
+        return self.user.username
+    
+# class Food(models.Model):
+#     image = models.ImageField(upload_to='profile_pic/')
+#     type = 
+#     category = 
+#     weight = option
+#     expiry date(optional)
+#     desc = 
---
+++ foodNet_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from foodNet_app import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+]
